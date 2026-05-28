2550f1b4c84452f9010a2e1354e64989a032b14b
nishadinfo09 <mdnishadali03@gmail.com>
2026-05-28 14:26:04 +0600

    get all deliveryPersons endpoint

--- app/api/delivery-person/route.ts
+++ app/api/delivery-person/route.ts
@@ -1,6 +1,7 @@
 import { db } from "@/lib/db/db";
-import { deliveryPersons } from "@/lib/db/schema";
+import { deliveryPersons, warehouses } from "@/lib/db/schema";
 import { deliveryPersonSchema } from "@/lib/validators/DeliveryPersonSchema";
+import { desc, eq } from "drizzle-orm";
 
 export async function POST(request: Request) {
   const data = await request.json();
@@ -25,6 +26,32 @@ export async function POST(request: Request) {
     console.log("failed to store deliveryPerson into database", error);
     return Response.json(
       { message: "failed to store deliveryPerson into database" },
+      { status: 500 },
+    );
+  }
+}
+
+export async function GET() {
+  try {
+    const allDeliveryPersons = await db
+      .select({
+        id: deliveryPersons.id,
+        name: deliveryPersons.name,
+        phone: deliveryPersons.phone,
+        warehouse: warehouses.name,
+      })
+      .from(deliveryPersons)
+      .leftJoin(warehouses, eq(deliveryPersons.id, warehouses.id))
+      .orderBy(desc(deliveryPersons.id));
+
+    return Response.json(
+      { message: "get all deliveryPerson successfully", allDeliveryPersons },
+      { status: 200 },
+    );
+  } catch (error) {
+    console.log("failed to get deliveryPerson into database", error);
+    return Response.json(
+      { message: "failed to get deliveryPerson into database" },
       { status: 500 },
     );
   }
